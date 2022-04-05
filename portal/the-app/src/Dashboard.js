e20078f5d97a7fc3846fd5ef315fde3a8e2c8368
jotpalch <jotp076315217@gmail.com>
2022-04-06 02:26:40 +0800

    homepage

--- portal/the-app/src/Dashboard.js
+++ portal/the-app/src/Dashboard.js
@@ -88,6 +88,99 @@ const Drawer = styled(MuiDrawer, {
   },
 }));
 
+const BgImg = () => {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        height: "100vh",
+        width: "100%",
+        left: 0,
+        top: 0,
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          height: 80,
+          color: "gray",
+          top: 0,
+          bottom: 0,
+          my: "auto",
+          ml: 45,
+        }}
+      >
+        <Typography variant="h2">
+          Physiological Data
+          <br /> Authentication Blockchain App
+        </Typography>
+      </Box>
+      <img
+        style={{ width: "100%" }}
+        src={require("./homepage_big.png")}
+        alt="homepage"
+      />
+    </Box>
+  );
+};
+
+const DataVis = () => {
+  return (
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Toolbar />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              <Chart />
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              <Deposits />
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              <Orders />
+            </Paper>
+          </Grid>
+        </Grid>
+        <Copyright sx={{ pt: 4 }} />
+      </Container>
+    </Box>
+  );
+};
+
 const mdTheme = createTheme();
 
 function DashboardContent() {
@@ -155,57 +248,8 @@ function DashboardContent() {
             {secondaryListItems}
           </List>
         </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
-        </Box>
+        <BgImg />
+        {/* <DataVis /> */}
       </Box>
     </ThemeProvider>
   );
