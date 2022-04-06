3f1db6e7eaf560dcad02b510f38ba0bbdb9f803b
jotpalch <jotp076315217@gmail.com>
2022-04-07 05:32:02 +0800

    api cors

--- portal/main.go
+++ portal/main.go
@@ -95,11 +95,30 @@ func main() {
 
 	server := gin.Default()
 	server.SetTrustedProxies(nil)
+	server.Use(CORSMiddleware())
 	server.LoadHTMLGlob("template/*")
 
 	route(server, client)
 
 	server.Run(":8877")
+}
+
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
 
 func route(server *gin.Engine, client *mongo.Client) {
