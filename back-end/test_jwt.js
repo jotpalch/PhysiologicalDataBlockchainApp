8e55eee5b4ec6cc7792a8947d077b8415847d2ed
botingchen <brad8910@gmail.com>
2022-04-09 16:31:34 +0800

    update respond to data requester

--- back-end/test_jwt.js
+++ back-end/test_jwt.js
@@ -7,17 +7,27 @@ const app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("/form",function(req,res){                          //廠商輸入需要之資料型態
+app.get("/form",function(req,res){                          //Data requester輸入需要之資料型態
     res.sendfile('back-end/views/form.html')
 });
 
-app.post('/signup',urlencodedParser,function(req, res) {   
+app.post('/signup',urlencodedParser,async function(req, res) {   
     let newmem = create_request(req.body.name,req.body.pubkey,req.body.collection_type)
-    const uri = "mongodb://admin:69251@ec2-34-221-6-169.us-west-2.compute.amazonaws.com:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=mongosh%201.3.0&directConnection=true&ssl=false";
-    const client = new MongoClient(uri);
-    let in_db = findOneListingByid(client,req.body.pubkey,req.body.collection_type);
-    console.log(in_db);
-    res.sendfile('back-end/views/signup.html');
+    let collect_detail = newmem.collection_type;
+    collect_detail_f = collect_detail.split(",");
+    var dataindb = new Array();
+    for(var i = 0;i<collect_detail_f.length;++i){
+        dataindb[i] = await CheckDataInDB(newmem.pubkey,collect_detail_f[i]);
+    }
+    let checker = arr => arr.every(v => v === true);
+    if(checker(dataindb)){
+        create_jwt(collect_detail_f);
+        res.sendfile('back-end/views/request_success.html')
+    }
+    else{
+        res.sendfile('back-end/views/request_fail.html');
+    }
+
 });
 
 app.listen(3000,function(){
@@ -38,9 +48,6 @@ function create_request(name,pubkey,collection_type){
     return a;
 }
 
-function check_data_in_db(collection_type){
-    
-}
 // async function main(){
 //     const uri = "mongodb://admin:69251@ec2-34-221-6-169.us-west-2.compute.amazonaws.com:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=mongosh%201.3.0&directConnection=true&ssl=false";
 //     const client = new MongoClient(uri);
@@ -73,28 +80,51 @@ function check_data_in_db(collection_type){
 
 
 // }
-main().catch(console.error);
-async function listDatabases(client){
-    const databaseList = await client.db().admin().listDatabases();
+// main().catch(console.error);
 
-    console.log("Database:");
-    databaseList.databases.forEach(db => {
-        console.log(db.name);
+// async function listDatabases(client){
+//     const databaseList = await client.db().admin().listDatabases();
+
+//     console.log("Database:");
+//     databaseList.databases.forEach(db => {
+//         console.log(db.name);
         
-    });
-}
-async function findOneListingByid(client,id_list,collection_type){
-    console.log(id_list);
-    console.log(collection_type);
-    for (var i = 0; i<collection_type.length; ++i){
-        const result = await client.db("users").collection(collection_type[i]).findOne( { DID_Public_Key : id_list });
+//     });
+// }
+async function CheckDataInDB(id_list,collection_type){
+    const uri = "mongodb://admin:69251@ec2-34-221-6-169.us-west-2.compute.amazonaws.com:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=mongosh%201.3.0&directConnection=true&ssl=false";
+    const client = new MongoClient(uri);
+    try{
+        await client.connect();
+        console.log(collection_type);
+        const result =  await client.db("users").collection(collection_type).findOne( { DID_Public_Key : id_list });
         if(result){
-            console.log(id_list);
-            console.log(result)
+            return true;
         }
         else{
-            console.log("no",collection_type[i],"data in database");
+            return false;
         }
+    }catch(e){
+        console.log(e);
+    }finally{
+        await client.close();
     }
 
 }
+function create_jwt(collection_type){
+    Header = {
+        "alg": "HS256",                              //產生簽章使用之演算法
+        "typ": "JWT"                                 //token種類
+    }
+    payload = {
+        "_id": "8KkAzQcZ2fOMmnNP7TfFOqCX1OteQ56y",   //欲蒐集之使用者的publickey
+        "collection_type" : collection_type          //欲蒐集之資料type
+    }
+    
+    // 設定密鑰
+    const SECRET = 'thisismynewproject'
+    // 建立 Token
+    const token = jwt.sign({ _id: payload._id.toString(),collection_type: payload.collection_type}, SECRET, { expiresIn: '1 day' })
+    console.log(token); 
+}
+
