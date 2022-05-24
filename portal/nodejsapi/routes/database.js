62f4b7ac3d3546bf6597279660db92bc80e844a3
jotpalch <jotp076315217@gmail.com>
2022-05-24 20:37:30 +0800

    renew api

--- portal/nodejsapi/routes/database.js
+++ portal/nodejsapi/routes/database.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 const mongo = require("mongodb").MongoClient;
 const url =
-  "mongodb://admin:69251@ec2-34-221-6-169.us-west-2.compute.amazonaws.com:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=mongosh%201.3.0&directConnection=true&ssl=false";
+  "mongodb://admin:69251@ec2-54-201-240-164.us-west-2.compute.amazonaws.com:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=mongosh%201.3.0&directConnection=true&ssl=false";
 let db = null;
+let data = null ;
 
 mongo.connect(url, async (err, client) => {
   if (err) {
     throw err;
   }
-  db = await client.db("RealTime_BigData");
+  db = await client.db("APPLE");
+  data = client.db("DATA");
   console.log("Database connected");
 });
 
@@ -23,17 +25,24 @@ router.get("/", (req, res) => {
     .end();
 });
 
-router.get("/:collection", async (req, res) => {
-  console.log(req.params.collection);
-  const col = db.collection(req.params.collection);
+router.get("/provider", async (req, res) => {
+  console.log(req.params.publickey);
+  const col = data.collection("Providers");
   const result = await col.find({}).toArray();
   res.status(200).send(result).end();
 });
 
-router.get("/:collection/:pk", async (req, res) => {
-  console.log(req.params.collection);
-  const col = db.collection(req.params.collection);
-  const result = await col.find({ DID_Public_Key: req.params.pk }).toArray();
+router.get("/:publickey", async (req, res) => {
+  console.log(req.params.publickey);
+  const col = db.collection(req.params.publickey);
+  const result = await col.find({}).toArray();
+  res.status(200).send(result).end();
+});
+
+router.get("/:publickey/:attr", async (req, res) => {
+  const col = db.collection(req.params.publickey);
+  // const result = await col.find({ name: req.params.attr }).toArray();
+  const result = await col.find({ [req.params.attr]: { $gt: 0 } }).toArray();
   res.status(200).send(result).end();
 });
 
