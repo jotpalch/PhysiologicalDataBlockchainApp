4528a69ea79e1045e449e76cb4d5015e0902d801
botingchen <Brad8910@gmail.com>
2022-06-12 01:19:40 +0800

    update TSP3-3

--- back-end/3-3_recieve_jwt_from_TSP.js
+++ back-end/3-3_recieve_jwt_from_TSP.js
@@ -6,3 +6,16 @@ const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get("/form",function(req,res){                          //Data requester輸入需要之資料型態
+    res.sendfile('views/form_for_require_data.html')
+});
+
+app.post('/data',urlencodedParser,async function(req, res) {   
+
+
+});
+
+app.listen(3001,function(){
+    console.log("server start at port 3001");
+});
